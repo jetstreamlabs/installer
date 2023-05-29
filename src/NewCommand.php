ac56ece5823404066983619060f90ae8040cf4ad
secondman <secondman@jetstreamlabs.com>
2023-05-29 03:17:10 -0700

    chore: styles
    Signed-off-by: secondman <secondman@jetstreamlabs.com>

--- src/NewCommand.php
+++ src/NewCommand.php
@@ -8,10 +8,7 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Question\ChoiceQuestion;
 use Symfony\Component\Console\Question\ConfirmationQuestion;
-use Symfony\Component\Console\Style\SymfonyStyle;
-use Symfony\Component\Process\Exception\ProcessFailedException;
 use Symfony\Component\Process\Process;
 
 class NewCommand extends Command
